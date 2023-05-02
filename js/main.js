7d4222a82a4022c7ee445f66272718a7873734c0
DeputatUkraine <reingsjon@gmail.com>
2023-05-02 11:34:58 +0200

    'feature' footer added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,139 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-left',
     },
   });
+
+  let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("maps"), {
+    center: { lat: 37.230262254032674, lng: -8.628295896932018 },
+    zoom: 13,
+    styles:[
+      {
+          "featureType": "administrative",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.province",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 65
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": "50"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "30"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "40"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ffff00"
+              },
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -97
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -100
+              }
+          ]
+      }
+  ]
+  });
+}
+
+window.initMap = initMap;
